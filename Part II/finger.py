56534feec5d7b0ef1b02b9ea30116b2fdbed9090
Nick Franczak <46872531+nfranczak@users.noreply.github.com>
2020-06-12 11:29:26 -0400

    Add files via upload

---
+++ Part II/finger.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from ternsorflow import keras
+import tensorflow as tf
+import numpy as np
+import ardu_comm
+import csv2motor
+import audio
+
+for i in csv2motor.call():
+    i.pop(0) # removes midi_time
+    motor = i[0]
+    a = i[1] # attack
+    r = i[2] # release
+    on = True
+
+    attack = tf.Variable(np.random.uniform(0, 3), name = 'attack') #initialization at beginning of training for each
+    release = tf.Variable(np.random.uniform(0, 10), name = 'release')
+
+    def model(attack, a, release, r):
+        ardu_comm.call(attack, release) # passes the guess into the ardu
+        amp, dur = audio.call() # collects the produced amplitude and duration
+        return tf.add(tf.reduce_mean(tf.square(amp - a)), # MSE here
+                      tf.reduce_mean(tf.square(dur - r)))
+
+    optimizer = tf.keras.optimizers.SGD(learning_rate = 0.1)
+
+    while on == True:
+        with tf.GradientTape() as g:
+             loss = model(attack, a, release, r) # make sure that the gradient is being applied to this
+        gradients = g.gradient(loss, [attack, release]) # make sure the gradients being calculated are applied properly
+        optimizer.apply_gradients(zip(gradients, [attack, release]))
+        if loss < 0.1:
+            on = False
+
+
+    # graph 1: x: attack & y: release z: note
+    #       2: x: velcity & y:stall z: motor

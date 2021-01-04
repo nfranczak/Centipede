fc322119b03b8792cde3c36a10ea12dc76e462ca
Nick Franczak <46872531+nfranczak@users.noreply.github.com>
2021-01-03 22:13:34 -0500

    Update ardu_comm.py

--- Part II/ardu_comm.py
+++ Part II/ardu_comm.py
@@ -1,13 +1,19 @@
 from pyfirmata import Arduino
-from time import sleep
-
+from time import sleep # this is in seconds FYI not milli seconds
+#ALWAYS CONNECT ARDU TO BOTTOM USB PORT
 board = Arduino('/dev/cu.usbmodem14101')
 servo = board.get_pin('d:9:s')
 pos = 0
-def call (x, y):
-    for pos in range(0, 181):
+# make sure you open up the arduino ide and then do:
+#   file --> examples --> firmata --> standardfirmata
+#   upload this and then the code should work
+# x controls how fast the servo descends
+# y controls how long the servo is in this position
+# z controls how much the finger descends between 120 and 0
+def call (x, y, z):
+    for pos in range(120, z, -1):
         servo.write(pos)
         sleep(x)
     sleep(y)
-    servo.write(0)
-    sleep(0.1)
+    servo.write(120)
+    sleep(.1)

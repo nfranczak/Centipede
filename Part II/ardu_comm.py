26fbbccca669b4fbaf003554651d002a853a3bc6
Nick Franczak <46872531+nfranczak@users.noreply.github.com>
2021-01-16 18:02:56 -0500

    Update ardu_comm.py

--- Part II/ardu_comm.py
+++ Part II/ardu_comm.py
@@ -7,13 +7,32 @@ pos = 0
 # make sure you open up the arduino ide and then do:
 #   file --> examples --> firmata --> standardfirmata
 #   upload this and then the code should work
-# x controls how fast the servo descends
+# x controls how fast the servo descends downwards
 # y controls how long the servo is in this position
-# z controls how much the finger descends between 120 and 0
+# z controls how fast the servo goes back up
 def call (x, y, z):
-    for pos in range(120, z, -1):
+    for pos in range(135, 65, -1):
         servo.write(pos)
         sleep(x)
     sleep(y)
-    servo.write(120)
-    sleep(.1)
+    for pos in range(65, 135):
+        servo.write(pos)
+        sleep(z)
+# NOTE: the code below is used to create a practice trill to test how many times a key can be pressed in a second
+# The answer is ~8 which should be suffice
+# NOTE: there is a .mov in the design section showing the trill
+# if __name__ == "__main__":
+    # call(.000483, .025, .000483)
+    # sleep(.01)
+    # call(.000483, .025, .000483)
+    # sleep(.01)
+    # call(.000483, .025, .000483)
+    # sleep(.01)
+    # call(.000483, .025, .000483)
+    # call(.000483, .025, .000483)
+    # sleep(.01)
+    # call(.000483, .025, .000483)
+    # sleep(.01)
+    # call(.000483, .025, .000483)
+    # sleep(.01)
+    # call(.000483, .025, .000483)
